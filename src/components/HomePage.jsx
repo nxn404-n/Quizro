985a17a36a90d9aa21e8e81334c1f77bd7a64611
nxn404-n <nafeurrahmanadil@gmail.com>
2024-09-01 15:31:11 +0600

    Add navbar and homepage demo texts

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Homepage!</h1>
+      <p>This is the main page of the website.</p>
+    </div>
+  );
+};
+export default HomePage;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,9 @@
+const NavBar = () => {
+  return (
+    <nav>
+      <h1>Quizro</h1>
+    </nav>
+  );
+};
+
+export default NavBar; 
