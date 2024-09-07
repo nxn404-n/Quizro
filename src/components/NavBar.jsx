0106752673bd403f8e5666b6de99daa7db41d30c
nxn404-n <nafeurrahmanadil@gmail.com>
2024-09-07 19:35:23 +0600

    Add styling to the navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,9 @@
 const NavBar = () => {
   return (
-    <nav>
-      <h1>Quizro</h1>
+    <nav className="bg-gray-300 pl-3 pt-2 pb-3">
+      <h1 className="font-custom text-3xl">Quizro</h1>
     </nav>
   );
 };
 
-export default NavBar; 
+export default NavBar;
